b0f6478cc5f8ae7c076b539596c777a701b11d63
Noel_Desv <noel@dataroots.io>
2020-08-03 23:10:44 +0200

    Crawling until formation

---
+++ crawl/kuleuven.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+import argparse
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from w3lib.html import remove_tags
+
+
+class KuleuvenSpider(scrapy.Spider):
+    name = "kuleuven"
+
+    def start_requests(self):
+        base_url = 'https://onderwijsaanbod.kuleuven.be/opleidingen/e/index.htm'
+        yield scrapy.Request(url=base_url, callback=self.parse)
+
+    def parse(self, response):
+        for href in response.xpath("//div[contains(@class, 'level_4')]//ul//li//a/@href").getall():
+            yield response.follow(href, self.parse_formation)
+
+    def parse_formation(self, response):
+        yield {'name': response.url}
+
+
+    def _cleanup(self, data):
+        if data is None:
+            return ""
+        elif isinstance(data, list):
+            result = list()
+            for e in data:
+                result.append(self._cleanup(e))
+            return result
+        else:
+            return remove_tags(data).strip()
+
+
+def main(output):
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': output
+    })
+
+    process.crawl(KuleuvenSpider)
+    process.start()  # the script will block here until the crawling is finished
+    print('All done.')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Crawl the Uantwerp courses catalog.')
+    parser.add_argument("--output", default="output.json".format(__file__), type=str, help="Output file")
+    parser.add_argument("--year", default="2020", type=str, help="Academic Year")
+    args = parser.parse_args()
+    main(args.output)
