44bd752da50f208c68c2f207dd8c5419fe080a25
Noel_Desv <noel@dataroots.io>
2020-11-15 18:53:02 +0100

    Put cleanup method in utils.py

--- crawl/settings.py
+++ crawl/settings.py
@@ -7,6 +7,14 @@ UGENT_URL="https://studiegids.ugent.be/" + YEAR + "/EN/FACULTY/faculteiten.html"
 KULEUVEN_URL = "https://onderwijsaanbod.kuleuven.be/opleidingen/e/index.htm"
 
 ABSOLUTE_PATH_DOWNLOAD_FOLDER = '/home/noel/Téléchargements/'
-SEPARATOR_FIRST_AND_SECOND_PART = 'Offered in the following programmes in  2020-2021crdtsoffering'
 
 DRIVER_PATH = "/snap/bin/chromium.chromedriver"
+
+# UGENT -------------
+SEPARATOR_FIRST_AND_SECOND_PART = 'Offered in the following programmes in  2020-2021crdtsoffering'
+
+LECTURERS_PARAGRAPH_SPLITTER = "Lecturers in academic year " + YEAR + "-" + str(int(YEAR)+1)
+OTHER_CATEGORIES_SEPARATORS = ["Course offerings and teaching methods",
+                               "Offered in the following programmes in",
+                               "Course size"]
+
--- crawl/utils.py
+++ crawl/utils.py
@@ -1,4 +1,5 @@
 import PyPDF2
+from w3lib.html import remove_tags
 
 def is_float(potential_float):
     try:
@@ -16,4 +17,17 @@ def read_pdf():
         page_content += pdfReader.getPage(page_nb).extractText()
 
     pdfFileObj.close()
+    with open("/home/noel/Téléchargements/A000186.txt", "w") as text_file:
+        text_file.write(page_content)
     return page_content
+
+    def cleanup(data):
+        if data is None:
+            return ""
+        elif isinstance(data, list):
+            result = list()
+            for e in data:
+                result.append(cleanup(e))
+            return result
+        else:
+            return remove_tags(data).strip()
