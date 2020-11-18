133ce1eedb0329d1f51ff8d0de484e5bfd55ab2e
aric.wizenberg <aric.wizenberg@inter.setec.fr>
2020-11-18 19:36:43 +0100

    Réparation utils.cleanup

--- crawl/config/utils.py
+++ crawl/config/utils.py
@@ -1,6 +1,7 @@
 import PyPDF2
 from w3lib.html import remove_tags
 
+
 def is_float(potential_float):
     try:
         float(potential_float)
@@ -8,26 +9,28 @@ def is_float(potential_float):
     except ValueError:
         return False
 
+
 def read_pdf():
-    pdfFileObj = open('/home/noel/Téléchargements/A000186.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdf_file_obj = open('/home/noel/Téléchargements/A000186.pdf', 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
 
     page_content = ''
-    for page_nb in range(pdfReader.numPages):
-        page_content += pdfReader.getPage(page_nb).extractText()
+    for page_nb in range(pdf_reader.numPages):
+        page_content += pdf_reader.getPage(page_nb).extractText()
 
-    pdfFileObj.close()
+    pdf_file_obj.close()
     with open("/home/noel/Téléchargements/A000186.txt", "w") as text_file:
         text_file.write(page_content)
     return page_content
 
-    def cleanup(data):
-        if data is None:
-            return ""
-        elif isinstance(data, list):
-            result = list()
-            for e in data:
-                result.append(cleanup(e))
-            return result
-        else:
-            return remove_tags(data).strip()
+
+def cleanup(data):
+    if data is None:
+        return ""
+    elif isinstance(data, list):
+        result = list()
+        for e in data:
+            result.append(cleanup(e))
+        return result
+    else:
+        return remove_tags(data).strip()
