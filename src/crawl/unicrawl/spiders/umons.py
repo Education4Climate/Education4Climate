ebf20cdb5e12325b783f77aabdf8bd81b3680c91
c3-antoine-qian <antoine.qian@c3iot.com>
2020-11-25 21:15:30 +0100

    Leave out evaluation and resources

--- src/crawl/unicrawl/spiders/umons.py
+++ src/crawl/unicrawl/spiders/umons.py
@@ -17,107 +17,117 @@ class UmonsSpider(scrapy.Spider):
             "https://web.umons.ac.be/fti-eii/fr/loffre-de-formations/",
             "https://web.umons.ac.be/fweg/fr/offre-de-formation/",
             "https://web.umons.ac.be/fau/fr/loffre-de-formation/",
-            "https://web.umons.ac.be/fmp/fr/loffre-de-formation-2/"
+            "https://web.umons.ac.be/fmp/fr/loffre-de-formation/"
             "https://web.umons.ac.be/fpms/fr/offre-de-formation/",
             "https://web.umons.ac.be/fpse/fr/offre-de-formation/",
             "https://web.umons.ac.be/eshs/fr/offre-de-formations/"
-            ]
-        # base_urls = 
+        ]
+        # base_urls =
         for url in base_urls:
             yield scrapy.Request(url=url, callback=self.parse_faculty, dont_filter=True)
-        
+
         # Only 1 program in the faculty of law
-        # Faculty of science website does not link its programs. 
+        # Faculty of science website does not link its programs.
         # Had to get them from a search form from the university page.
-        program_urls = ["https://web.umons.ac.be/droit/fr/training-offer/ba-bdroit/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scbioc-c/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scbiol/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scchim/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scinfo/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m1-biol60/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefd/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefs/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfs/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfd/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m1-chim60/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m1-info60-c/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-inffsa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-infofs/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-infofa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-infosp-c/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m1-info60/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-mathfa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-matfsf/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-matfsi/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-mathfd/",
-        'https://web.umons.ac.be/fs/fr/training-offer/m1-math60/',
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-physfa/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-physfd/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m1-phys60/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scmath/",
-        "https://web.umons.ac.be/fs/fr/training-offer/ba-scphys/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfd/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfs/",
-        "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfa/"]
+        program_urls = [
+            "https://web.umons.ac.be/droit/fr/training-offer/ba-bdroit/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scbioc-c/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scbiol/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scchim/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scinfo/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m1-biol60/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefd/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefs/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bioefa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfs/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfd/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m1-chim60/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-chimfa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m1-info60-c/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-inffsa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-infofs/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-infofa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-infosp-c/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m1-info60/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-mathfa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-matfsf/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-matfsi/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-mathfd/",
+            'https://web.umons.ac.be/fs/fr/training-offer/m1-math60/',
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-physfa/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-physfd/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m1-phys60/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scmath/",
+            "https://web.umons.ac.be/fs/fr/training-offer/ba-scphys/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfd/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfs/",
+            "https://web.umons.ac.be/fs/fr/training-offer/m2-bbmcfa/"
+        ]
         for url in program_urls:
             yield scrapy.Request(url=url, callback=self.parse_prog)
 
-    def parse(self, response):
-        pass
-
     def parse_faculty(self, response):
         # bachelor_string_1 = "First cycle : Bachelor’s degree"
-        bachelor_string = "1er Cycle: Bachelier"
-        # masters_string_1 = "Second cycle : Master’s degree"
-        masters_string= "2ème Cycle: Master"
+        bachelor_string = "1er Cycle : Bachelier"
+        bachelor_string_bis = "1er cycle : Bachelier"
+        masters_string = "2ème Cycle : Master"
+        masters_string_bis = "2ème cycle : Master"
 
         # Leaving out PhDs, formations for adults and other special formations.
-        bachelors = response.xpath(f'//li[a/text()="{bachelor_string}"]/ul/li/a/@href').getall()
-        # if not bachelors:
-        #     bachelors = response.xpath(f'//li[a/text()="{bachelor_string_2}"]/ul/li/a/@href').getall()
-        
-        masters = response.xpath(f'//li[a/text()="{masters_string}"]/ul/li/a/@href').getall()
-        # if not masters:
-        #     masters = response.xpath(f'//li[a/text()="{masters_string_2}"]/ul/li/a/@href').getall()
+        bachelors = response.xpath(
+            f'//li[a/text()="{bachelor_string}"]/ul/li/a/@href').getall()
+        if not bachelors:
+            bachelors = response.xpath(
+                f'//li[a/text()="{bachelor_string_bis}"]/ul/li/a/@href').getall()
+
+        masters = response.xpath(
+            f'//li[a/text()="{masters_string}"]/ul/li/a/@href').getall()
+        if not masters:
+            masters = response.xpath(
+                f'//li[a/text()="{masters_string_bis}"]/ul/li/a/@href').getall()
 
         if not bachelors:
-            bachelors_details = response.xpath(f'//a[text()="{bachelor_string}"]/@href').get()
-            # if not bachelors_details:
-            #     bachelors_details = response.xpath(f'//a[text()="{bachelor_string_2}"]/@href').get()
+            bachelors_details = response.xpath(
+                f'//a[text()="{bachelor_string}"]/@href').get()
+            if not bachelors_details:
+                bachelors_details = response.xpath(
+                    f'//a[text()="{bachelor_string_bis}"]/@href').get()
             yield response.follow(bachelors_details, self.parse_details)
         else:
             for bachelor in bachelors:
                 yield response.follow(bachelor, self.parse_prog)
 
         if not masters:
-            masters_details = response.xpath(f'//a[text()="{masters_string}"]/@href').get()
-            # if not masters_details:
-            #     masters_details = response.xpath(f'//a[text()="{masters_string_2}"]/@href').get()
+            masters_details = response.xpath(
+                f'//a[text()="{masters_string}"]/@href').get()
+            if not masters_details:
+                masters_details = response.xpath(
+                    f'//a[text()="{masters_string_bis}"]/@href').get()
             yield response.follow(masters_details, self.parse_details)
         else:
             for master in masters:
                 yield response.follow(master, self.parse_prog)
-        
+
     def parse_details(self, response):
-        programs = response.xpath('//article[starts-with(@class,"shortcode-training training-small scheme-")]/a/@href').getall()
+        programs = response.xpath(
+            '//article[starts-with(@class,"shortcode-training training-small scheme-")]/a/@href').getall()
         for program in programs:
             yield response.follow(program, self.parse_prog)
 
     def parse_prog(self, response):
-        href = response.xpath('//a[contains(@class, "button-primary-alt scheme-background scheme-background-hover")]/@href').get()
+        href = response.xpath(
+            '//a[contains(@class, "button-primary-alt scheme-background scheme-background-hover")]/@href').get()
         if not href:
             yield response.follow(response.url, self.parse_details, dont_filter=True)
         else:
-            location = response.xpath('//div[div/text()="Lieu"]').css('div.value::text').get()
-            yield response.follow(url=href, callback=self.parse_prog_detail, cb_kwargs={'location':location})  
+            location = response.xpath(
+                '//div[div/text()="Lieu"]').css('div.value::text').get()
+            yield response.follow(url=href, callback=self.parse_prog_detail, cb_kwargs={'location': location})
 
     def parse_prog_detail(self, response, location):
-        faculty = response.css('td.facTitle::text')
+        faculty = response.css('td.facTitle::text').get()
         for href in response.css('a.linkue::attr(href)').getall():
             yield response.follow(url=href, callback=self.parse_course, cb_kwargs={'location': location, 'faculty': faculty})
-        
 
     @staticmethod
     def parse_course(response, location, faculty):
@@ -131,12 +141,11 @@ class UmonsSpider(scrapy.Spider):
             'teachers':     u.cleanup(response.css('table.UETbl')[0].css('li::text').get()),
             'language':     u.cleanup(response.css('table.UETbl')[1].css('li::text').get()),
             'prerequisite': u.cleanup(response.xpath('//div[p/text() = "Compétences préalables"]/p[@class="texteRubrique"]').get()),
-            'goal':        u.cleanup(response.xpath('//div[p/text() = "Acquis d\'apprentissage UE"]/p[@class="texteRubrique"]').get()),
+            'goal':         u.cleanup(response.xpath('//div[p/text() = "Acquis d\'apprentissage UE"]/p[@class="texteRubrique"]').get()),
             'content':      u.cleanup(response.xpath('//div[p/text() = "Contenu de l\'UE"]/p[@class="texteRubrique"]').get()),
-            # 'method':       u.cleanup(response.xpath("normalize-space(.//div[div[contains(text(),'Méthodes')]]/div[@class='col-sm-10 fa_cell_2'])").get()),
-            # 'evaluation':   u.cleanup(response.xpath("normalize-space(.//div[div[contains(text(),'Modes')]]/div[@class='col-sm-10 fa_cell_2'])").get()),
-            # 'other':        u.cleanup(response.xpath("normalize-space(.//div[div[contains(text(),'Autres')]]/div[@class='col-sm-10 fa_cell_2'])").get()),
-            # 'resources':    u.cleanup(response.xpath("normalize-space(.//div[div[contains(text(),'Ressources')]]/div[@class='col-sm-10 fa_cell_2'])").get()),
+            'method':       u.cleanup(response.xpath('//div[p/text() = "Mode d\'enseignement"]/p[@class="texteRubrique"]').get()),
+            # 'evaluation':   TODO:,
+            # 'resources':    u.cleanup(response.xpath('//tbody[tr/th/text()="Autres références conseillées"]').get()),
             'faculty':      u.cleanup(faculty),
             'url':          response.url
         }
