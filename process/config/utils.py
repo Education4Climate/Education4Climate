ab60e2930752114a061f6e698689118eafd2212b
Noel_Desv <noel@dataroots.io>
2020-11-27 01:00:51 +0100

    Solve tokenize callback function issue due to argument

--- process/config/utils.py
+++ process/config/utils.py
@@ -2,8 +2,9 @@ from unidecode import unidecode
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import spacy
+from functools import partial
 
-def tokenize(text, nlp):
+def tokenize(nlp, text):
     return [unidecode(w.text.lower()) for w in nlp(text) if w.pos_ not in ["PUNCT","NUM","SYM","SPACE"] and w.is_stop is False]
 
 def refactor_pattern(x):
@@ -15,7 +16,8 @@ def load_models(corpus, language):
     # Construction/entrainement des modeles
     nlp = spacy.load(language)
     print("training")
-    vectorizer = TfidfVectorizer(tokenizer=tokenize, analyzer="word", ngram_range=(1, 3))
+    t = partial(tokenize, nlp) # Moyen d'envoyer un argument (nlp) dans une fonction callback
+    vectorizer = TfidfVectorizer(tokenizer=t, analyzer="word", ngram_range=(1, 3))
     vectorizer.fit(corpus)
     #pickle.dump(vectorizer, open(cfg.vectorizer.format(language), "wb"))
     features = vectorizer.get_feature_names()
