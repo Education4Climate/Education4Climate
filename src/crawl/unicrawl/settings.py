aac2ffa9396d3496c81ad5d61ee5eb9bb8d12201
Icarwiz <icarwiz@yahoo.fr>
2020-12-04 00:19:42 +0100

    Paramétrage du crawler

--- src/crawl/unicrawl/settings.py
+++ src/crawl/unicrawl/settings.py
@@ -14,20 +14,22 @@ BOT_NAME = 'unicrawl'
 SPIDER_MODULES = ['unicrawl.spiders']
 NEWSPIDER_MODULE = 'unicrawl.spiders'
 
+FEED_FORMAT = 'json'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'unicrawl (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
