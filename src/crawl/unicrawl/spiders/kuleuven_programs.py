149568b97a68c5c4b1eaa670439165e0ac8a4414
duboisa1 <antoine-dubois@hotmail.com>
2021-01-10 13:53:47 +0100

    Divided KUL crawler into two (programs and courses), still need to crawl courses fully

--- src/crawl/unicrawl/spiders/kuleuven_programs.py
+++ src/crawl/unicrawl/spiders/kuleuven_programs.py
@@ -1,33 +1,34 @@
-import re
-
-import src.crawl.unicrawl.spiders.config.settings as s
-import utils as u
-
-
-# Read PDF file
-page_content = u.read_pdf()
-
-
-
 from abc import ABC
 
 import scrapy
 
-BASE_URL = f"https://studiegids.ugent.be/{s.YEAR}/EN/FACULTY/faculteiten.html"
+from config.settings import YEAR
+
+BASE_URL = 'https://onderwijsaanbod.kuleuven.be/opleidingen/n/'
 
 
-class UgentCourseSpider(scrapy.Spider, ABC):
-    name = 'ugent-programs'
+class KULeuvenProgramSpider(scrapy.Spider, ABC):
+    name = 'kuleuven-programs'
     custom_settings = {
-        'FEED_URI': f'../../data/crawling-output/ugent_programs_{s.YEAR}.json',
+        'FEED_URI': f'../../data/crawling-output/kuleuven_programs_{YEAR}.json',
     }
 
     def start_requests(self):
         yield scrapy.Request(BASE_URL, self.parse_main)
 
     def parse_main(self, response):
-        for href in response.xpath("//aside//li[a[@target='_top']]/a"):
-            yield response.follow(href, self.parse_program_group, cb_kwargs={"base_dict": base_dict})
+
+        # 'Faculties'
+        faculties = response.xpath("//h3/a/text()").getall()
+
+        # Get program groups for each faculty
+        for faculty in faculties:
+            program_group_links = response.xpath(f"//h3/a[text()='{faculty}']/following::div[1]"
+                                                 f"//li[contains(@class, 'taal_n') or "
+                                                 f"contains(@class, 'taal_e')]/a/@href").getall()
+            for link in program_group_links:
+                base_dict = {"faculty": faculty}
+                yield response.follow(link, self.parse_program_group, cb_kwargs={"base_dict": base_dict})
 
     def parse_program_group(self, response, base_dict):
 
@@ -40,20 +41,37 @@ class UgentCourseSpider(scrapy.Spider, ABC):
 
     @staticmethod
     def parse_program(response, base_dict):
+
         program_name = response.xpath("//h1/text()").get()
         campus = ''
         if '(' in program_name:
             campus = program_name.split('(')[-1].split(')')[0]
-        cycle = ''
+        cycle = 'other'
         if 'bachelor' in program_name or 'Bachelor' in program_name:
             cycle = 'bac'
+        elif 'postgraduaat' in program_name or 'Postgraduaat' in program_name:
+            cycle = 'postgrad'
         elif 'graduaat' in program_name or 'Graduaat' in program_name:
             cycle = 'grad'
+        elif 'master' in program_name or 'Master' in program_name:
+            cycle = 'master'
+        elif 'doctor' in program_name or 'Doctor' in program_name:
+            cycle = 'doctor'
+
         courses = response.xpath("//tr[contains(@class, 'opo_row')]//td[@class='code']/text()").getall()
+        # If there are no courses to crawl, discard the program
+        if len(courses) == 0:
+            return
         courses_url = response.xpath("//td[@class='opleidingsonderdeel']/a/@href").getall()
         courses_url = [c.split("/syllabi/")[1].split('.htm')[0] for c in courses_url]
+
+        def convert_ects_str(ects_str: str):
+            if ects_str == '':
+                return None
+            return float(ects_str) if '.' in ects_str else int(ects_str)
         ects = response.xpath("//tr[contains(@class, 'opo_row')]//td[@class='sp']/text()").getall()
-        ects = [int(e.split(" ")[0]) for e in ects]
+        ects = [convert_ects_str(e.split(" ")[0]) for e in ects]
+
         # Get list of unique courses and ects
         courses, courses_url, ects = zip(*set(zip(courses, courses_url, ects)))
 
@@ -65,6 +83,3 @@ class UgentCourseSpider(scrapy.Spider, ABC):
                     'courses_urls': courses_url}
 
         yield {**base_dict, **cur_dict}
-
-
-
