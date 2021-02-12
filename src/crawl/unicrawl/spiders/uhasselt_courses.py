b0073b9b0d09d401ec96910027563056c6fe2573
duboisa1 <antoine-dubois@hotmail.com>
2021-02-12 18:38:19 +0100

    Minor modification

--- src/crawl/unicrawl/spiders/uhasselt_courses.py
+++ src/crawl/unicrawl/spiders/uhasselt_courses.py
@@ -23,16 +23,30 @@ class UHasseltCourseSpider(scrapy.Spider, ABC):
         courses_ids = pd.read_json(open(PROG_DATA_PATH, "r"))["courses"]
         courses_ids_list = sorted(list(set(courses_ids.sum())))
 
+        i = 0
         for course_id in courses_ids_list:
             yield scrapy.Request(BASE_URL.format(course_id, YEAR), self.parse_main)
+            if i == 4:
+                return
+            i += 1
+
+    # TODO: need to do a POST request too?
 
     @staticmethod
     def parse_main(response):
 
+        id = ''
+        course_name = ''
+        year = ''
+        teachers = response.xpath("//td[contains(text(), 'lecturer')]/following::td[1]/a/text()").getall()
+        teachers += response.xpath("//td[contains(text(), 'lecturer')]/following::td[1]/a/text()").getall()
+        languages = ''
+        content = ''
+
         data = {
-            'id': short_name,
-            'name': class_name,
-            'year': years,
+            'id': id,
+            'name': course_name,
+            'year': year,
             'teacher': teachers,
             'language': languages,
             'url': response.url,
