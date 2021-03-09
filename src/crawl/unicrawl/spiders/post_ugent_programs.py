29272ead7709774e861d9399d8ae1019b4d6383c
Noël SCHNEIDER <noel.desvoyes@gmail.com>
2021-03-09 10:00:03 +0100

    Generating programs

--- src/crawl/unicrawl/spiders/post_ugent_programs.py
+++ src/crawl/unicrawl/spiders/post_ugent_programs.py
@@ -18,24 +18,27 @@ def main():
 
     # Generating programs file
     programs_and_courses_data = data.groupby(["id", "name", "cycle", "url"]).agg({"courses": sum,
-                                                                      "courses_teacher": sum,
-                                                                      "courses_ects": sum,
-                                                                      "courses_content": sum
-                                                                      }).reset_index()
-    #print(programs_and_courses_data)
-    PROGRAM_COLUMNS = ["id", "name", "cycle", "url", "courses"]
-    programs_and_courses_data[PROGRAM_COLUMNS].to_json(path_or_buf=UGENT_NEW_CRAWLED_PATH_PROGRAMS, orient='records')
+                                                                                  "courses_teacher": sum,
+                                                                                  "courses_ects": sum,
+                                                                                  "courses_content": sum
+                                                                                  }).reset_index()
+    # print(programs_and_courses_data)
+    PROGRAM_COLUMNS = ["id", "name", "cycle", "url", "courses", "courses_ects"]
+    print(programs_and_courses_data[PROGRAM_COLUMNS])
+    programs_and_courses_data[PROGRAM_COLUMNS] \
+        .rename(columns={col: col.removeprefix('courses_') for col in PROGRAM_COLUMNS}) \
+        .to_json(path_or_buf=UGENT_NEW_CRAWLED_PATH_PROGRAMS, orient='records')
 
     # Generating courses file
-    courses_columns = [col for col in data.columns if col.startswith('courses')]
+    COURSES_COLUMNS = [col for col in data.columns if col.startswith('courses')]
     courses_data = []
     for program_row in programs_and_courses_data.iterrows():
         if program_row[1]['courses']:
             for i in range(len(program_row[1]['courses'])):
-                course_row = [program_row[1][col][i] for col in courses_columns]
+                course_row = [program_row[1][col][i] for col in COURSES_COLUMNS]
                 courses_data.append(course_row)
 
-    new_columns = [col.removeprefix('courses_') for col in data.columns if col.startswith("courses")]
+    new_columns = [col.removeprefix('courses_') for col in COURSES_COLUMNS]
     print("new_columns: {}".format(new_columns))
     courses_df = pd.DataFrame(columns=new_columns,
                               data=courses_data)
