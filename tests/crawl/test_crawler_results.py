f1966603f0ac12e0a704b89e704412c85ccb7c7e
Noël SCHNEIDER <noel.desvoyes@gmail.com>
2021-03-12 16:57:14 +0100

    Implementing column-based tests

--- tests/crawl/test_crawler_results.py
+++ tests/crawl/test_crawler_results.py
@@ -2,6 +2,9 @@ import unittest
 import pandas as pd
 import glob
 
+REQUIRED_COLUMNS_COURSES = {"name", "id", "teachers", "ects", "content", "cycle", "languages", "url", "year", "campus", "faculty", "program"}
+REQUIRED_COLUMNS_PROGRAMS = {"name", "id", "cycle", "campus", "faculty", "courses", "ects"}
+
 
 class TestTypesFieldsInCrawledFiles(unittest.TestCase):
 
@@ -12,6 +15,15 @@ class TestTypesFieldsInCrawledFiles(unittest.TestCase):
             except Exception as e:
                 self.fail("The following exception: {exception}\n"
                           "has been thrown on this file: {file}".format(exception=e, file=file))
+
+    def test_required_columns_are_in_courses_files(self):
+        for file in glob.glob("data/crawling-output/*_courses_*.json"):
+            with self.subTest(file=file):
+                columns = set(pd.read_json(file).columns)
+                if not REQUIRED_COLUMNS_COURSES.issubset(columns):
+                    self.fail("The following columns are missing in the file {file}\n"
+                              "{missing_columns}".format(file=file,
+                                                         missing_columns=REQUIRED_COLUMNS_COURSES.difference(columns)))
 
     """
     def test_field_names(self):
