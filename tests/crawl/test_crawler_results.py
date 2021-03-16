68d561b1de9b2f8aa24663442e5e2f201ccd7cf2
Noël SCHNEIDER <noel.desvoyes@gmail.com>
2021-03-16 16:32:26 +0100

    Adding type-based tests

--- tests/crawl/test_crawler_results.py
+++ tests/crawl/test_crawler_results.py
@@ -1,9 +1,18 @@
 import unittest
 import pandas as pd
 import glob
+import sys
+from typing import List
 
-REQUIRED_COLUMNS_COURSES = {"name", "id", "teachers", "ects", "content", "cycle", "languages", "url", "year", "campus", "faculty", "program"}
+REQUIRED_COLUMNS_COURSES = {"name", "id", "teachers", "ects", "content", "cycle", "languages", "url", "year", "campus",
+                            "faculty", "program"}
 REQUIRED_COLUMNS_PROGRAMS = {"name", "id", "cycle", "campus", "faculty", "courses", "ects"}
+
+TYPES_COLUMNS_COURSES = {"name": str, "id": str, "teachers": list, "ects": list, "content": str,
+                         "cycle": str, "languages": list, "url": str, "year": int, "campus": str,
+                         "faculty": str, "program": str}
+TYPES_COLUMNS_PROGRAMS = {"name": str, "id": str, "cycle": str, "campus": str, "faculty": str, "courses": list,
+                          "ects": list}
 
 
 class TestTypesFieldsInCrawledFiles(unittest.TestCase):
@@ -16,30 +25,77 @@ class TestTypesFieldsInCrawledFiles(unittest.TestCase):
                 self.fail("The following exception: {exception}\n"
                           "has been thrown on this file: {file}".format(exception=e, file=file))
 
-    def test_required_columns_are_in_courses_files(self):
+    def test_required_columns_are_in_course_files(self):
         for file in glob.glob("data/crawling-output/*_courses_*.json"):
             with self.subTest(file=file):
                 columns = set(pd.read_json(file).columns)
-                if not REQUIRED_COLUMNS_COURSES.issubset(columns):
-                    self.fail("The following columns are missing in the file {file}\n"
-                              "{missing_columns}".format(file=file,
-                                                         missing_columns=REQUIRED_COLUMNS_COURSES.difference(columns)))
+                self.assertTrue(REQUIRED_COLUMNS_COURSES.issubset(columns),
+                                msg="\n\nThe following columns are missing in the file >>> {file} <<<\n"
+                                    "====> {missing_columns}".format(file=file,
+                                                                     missing_columns=REQUIRED_COLUMNS_COURSES.difference(
+                                                                         columns)))
 
-    """
-    def test_field_names(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_required_columns_are_in_program_files(self):
+        for file in glob.glob("data/crawling-output/*_programs_*.json"):
+            with self.subTest(file=file):
+                columns = set(pd.read_json(file).columns)
+                self.assertTrue(columns.issubset(REQUIRED_COLUMNS_PROGRAMS),
+                                msg="\n\nThe following columns are missing in the file >>> {file} <<<\n"
+                                    "====> {missing_columns}".format(file=file,
+                                                                     missing_columns=REQUIRED_COLUMNS_PROGRAMS.difference(
+                                                                         columns)))
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_too_many_columns_in_program_files(self):
+        for file in glob.glob("data/crawling-output/*_programs_*.json"):
+            with self.subTest(file=file):
+                columns = set(pd.read_json(file).columns)
+                self.assertTrue(REQUIRED_COLUMNS_PROGRAMS.issubset(columns),
+                                msg="\n\nThe following columns are not required in the file >>> {file} <<<\n"
+                                    "====> {not_required_columns}".format(file=file,
+                                                                          not_required_columns=columns.difference(
+                                                                              REQUIRED_COLUMNS_PROGRAMS)))
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-    """
+    def test_too_many_columns_in_course_files(self):
+        for file in glob.glob("data/crawling-output/*_courses_*.json"):
+            with self.subTest(file=file):
+                columns = set(pd.read_json(file).columns)
+                self.assertTrue(REQUIRED_COLUMNS_COURSES.issubset(columns),
+                                msg="\n\nThe following columns are not required in the file >>> {file} <<<\n"
+                                    "====> {not_required_columns}".format(file=file,
+                                                                          not_required_columns=columns.difference(
+                                                                              REQUIRED_COLUMNS_COURSES)))
+
+    def test_types_in_program_files(self):
+        for file in glob.glob("data/crawling-output/*_programs_*.json"):
+            with self.subTest(file=file):
+                data = pd.read_json(file)
+                for column in data.columns:
+                    with self.subTest(column=column):
+                        for value in data[column].values:
+                            self.assertTrue(type(value) == TYPES_COLUMNS_PROGRAMS[column],
+                                            msg="\n\nIn the column  of the file {file}"
+                                                "\nThe expecting type of '{column}' is >> {exp_type} << "
+                                                "but a value with type >> {act_type} << "
+                                                "has been found".format(column=column,
+                                                                        file=file,
+                                                                        exp_type=TYPES_COLUMNS_PROGRAMS[column],
+                                                                        act_type=type(value)))
+
+    def test_types_in_course_files(self):
+        for file in glob.glob("data/crawling-output/*_courses_*.json"):
+            with self.subTest(file=file):
+                data = pd.read_json(file)
+                for column in data.columns:
+                    with self.subTest(column=column):
+                        for value in data[column].values:
+                            self.assertTrue(type(value) == TYPES_COLUMNS_COURSES[column],
+                                            msg="\n\nIn the column  of the file {file}"
+                                                "\nThe expecting type of '{column}' is >> {exp_type} << "
+                                                "but a value with type >> {act_type} << "
+                                                "has been found".format(column=column,
+                                                                        file=file,
+                                                                        exp_type=TYPES_COLUMNS_COURSES[column],
+                                                                        act_type=type(value)))
 
 
 if __name__ == '__main__':
