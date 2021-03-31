bc247bc371822fa4d563223c7cb085fc88a0ab29
thehappysloth <73159162+thehappysloth@users.noreply.github.com>
2021-03-31 20:05:36 +0200

    FIX : forgot to add the ?lang=xx in the menu

--- docs/js/teachers.js
+++ docs/js/teachers.js
@@ -34,7 +34,8 @@ var app = Vue.createApp({
             alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
             firstLetterSearched: "",
             searchedName: "",
-            selectedThemes: []
+            selectedThemes: [],
+            errors: ""
         };
     },
     computed: {
