52f3ab2cd88febc338f3e070685fbe8b0d7e7835
duboisa1 <antoine-dubois@hotmail.com>
2021-04-06 17:32:11 +0200

    Updated UGent

--- src/crawl/unicrawl/spiders/ugent_courses.py
+++ src/crawl/unicrawl/spiders/ugent_courses.py
@@ -1,71 +1,81 @@
 # -*- coding: utf-8 -*-
 from abc import ABC
 from pathlib import Path
+from os import remove
+import json
 
+import numpy as np
 import pandas as pd
 import scrapy
 
+import urllib3
+from pdfminer.high_level import extract_text
+
+
 from config.settings import YEAR
-from config.utils import cleanup
 
 BASE_URl = "https://studiegids.ugent.be/2020/NL/studiefiches/{}.pdf"
 PROG_DATA_PATH = Path(__file__).parent.absolute().joinpath(
     f'../../../../data/crawling-output/ugent_programs_{YEAR}.json')
 
-# TODO: check languages
-LANGUAGES_DICT = {"Nederlands": 'nl',
-                  "Dutch": 'nl',
-                  "Frans": 'fr',
-                  "French": 'fr',
-                  "Français": 'fr',
-                  "Engels": 'en',
-                  "English": 'en',
-                  "Deutsch": 'de',
-                  "German": 'de',
-                  "Duits": 'de',
-                  "Spanish": 'es',
-                  "Spaans": 'es',
-                  "Español": 'es',
-                  "Italian": 'it',
-                  "Italiaans": 'it',
-                  "Italiano": 'it',
-                  "Polish": 'pl',
-                  "Pools": 'pl',
-                  "Russisch": 'ru',
-                  "Arabic": 'ar',
-                  "Arabisch": 'ar',
-                  "Japans": 'jp',
-                  "Chinees": 'cn',
-                  'Korean': 'kr'}
+
+def download_pdf(pdf_url: str) -> str:
+    urllib3.disable_warnings()
+    filePath = r"file.pdf"
+    with urllib3.PoolManager() as http:
+        r = http.request('GET', pdf_url)
+        with open(filePath, 'wb') as fout:
+            fout.write(r.data)
+    return filePath
 
 
-class UGentCourseSpider(scrapy.Spider, ABC):
-    name = "ugent-courses"
-    custom_settings = {
-        'FEED_URI': Path(__file__).parent.absolute().joinpath(
-            f'../../../../data/crawling-output/ugent_courses_{YEAR}.json')
-    }
-
-    def start_requests(self):
-
-        courses_ids = pd.read_json(open(PROG_DATA_PATH, "r"))["courses"]
-        courses_ids_list = sorted(list(set(courses_ids.sum())))
-
-        print(len(courses_ids_list))
-        for course_id in courses_ids_list:
-            yield scrapy.Request(BASE_URl.format(course_id), self.parse_main, cb_kwargs={'course_id': course_id})
-
-    @staticmethod
-    def parse_main(response, course_id):
+def extract_content(pdf_url: str) -> str:
+    pdf_path = download_pdf(pdf_url)
+    content = extract_text(pdf_path)
+    remove(pdf_path)
+    return content
 
 
+def main(fn):
 
-        yield {
+    courses_df = pd.read_json(open(PROG_DATA_PATH, "r"))[["courses", "courses_names", "courses_teachers"]]
+    courses_ids_list = courses_df['courses'].sum()
+    courses_names_list = courses_df['courses_names'].sum()
+    courses_teachers_list = courses_df['courses_teachers'].sum()
+    unique_courses_ids_list = sorted(list(set(courses_ids_list)))
+
+    data = []
+    for i, course_id in enumerate(unique_courses_ids_list):
+        if i % 10 == 0:
+            print(course_id, f'{i}/{len(unique_courses_ids_list)}')
+        courses_ids_positions = [i for i, c in enumerate(courses_ids_list) if c == course_id]
+        # Find name of the course
+        course_name = np.array(courses_names_list)[courses_ids_positions][0]
+        # Find teachers giving the course
+        unique_teachers = list(set(np.array(courses_teachers_list)[courses_ids_positions]))
+        unique_teachers = [t for t in unique_teachers if t is not None and 'N.' not in t]
+        # Languages
+        languages = ['nl']
+        if '[' in course_name and ']' in course_name:
+            languages = course_name.split("[")[-1].split(']')[0].split(',')
+        # Content
+        url = BASE_URl.format(course_id)
+        content = extract_content(url)
+
+        data += [{
             'id': course_id,
             'name': course_name,
-            'year': years,
-            'teacher': teachers,
-            'language': languages,
-            'content': content,
-            'url': response.url
-        }
+            'year': f"{YEAR}-{int(YEAR) + 1}",
+            'teachers': unique_teachers,
+            'languages': languages,
+            'url': url,
+            'content': content
+        }]
+
+    data = pd.DataFrame.from_dict(data)
+    data.to_json(fn, orient='records')
+
+
+if __name__ == '__main__':
+    FEED_URI = Path(__file__).parent.absolute().joinpath(f'../../../../data/crawling-output/ugent_courses_{YEAR}.json')
+    main(FEED_URI)
--- src/crawl/unicrawl/spiders/ugent_programs.py
+++ src/crawl/unicrawl/spiders/ugent_programs.py
@@ -14,70 +14,6 @@ import urllib3
 BASE_URL = "https://studiegids.ugent.be/{year}/NL/FACULTY/{faculty}/{cycle}/{cycle}.html"
 
 
-def extract_id_from_url_ugent(url: str) -> str:
-    split_url = re.findall(r"[\w']+", url)
-    return split_url[split_url.index("pdf") - 1]
-
-
-def extract_url_from_toggle_content(toggle_content: str) -> str:
-    url = toggle_content.split(",")[1].replace("'", "")
-    return url
-
-
-def extract_ects(ects_list: List[str]) -> List[str]:
-    if "Crdt" in ects_list:
-        ects_list.remove("Crdt")
-
-    if "" in ects_list:
-        ects_list.remove("")
-    return ects_list
-
-
-def extract_year(messy_string: str) ->str:
-    year = "".join([j for j in messy_string if j.isdigit()])
-    year = year[:4] + '-' + year[4:]
-    return year
-
-
-def extract_teacher(teacher_list: List[str]) -> List[str]:
-    if "Instructor" in teacher_list:
-        teacher_list.remove("Instructor")
-
-    if "" in teacher_list:
-        teacher_list.remove("")
-    return teacher_list
-
-
-def read_pdf(pdf_path: str) -> str:
-    page_content = ''
-    with pdfplumber.open(pdf_path) as pdf:
-        for page_nb in range(len(pdf.pages)):
-            first_page = pdf.pages[0]
-            page_content += first_page.extract_text()
-    return page_content
-
-
-def download_pdf(pdf_url: str) -> str:
-    urllib3.disable_warnings()
-    filePath = r"file.pdf"
-    with urllib3.PoolManager() as http:
-        r = http.request('GET', pdf_url)
-        with open(filePath, 'wb') as fout:
-            fout.write(r.data)
-    return filePath
-
-
-def delete_pdf(pdf_path: str) -> str:
-    os.remove(pdf_path)
-
-
-def extract_content(pdf_url: str) -> str:
-    pdf_path = download_pdf(pdf_url)
-    content = read_pdf(pdf_path)
-    delete_pdf(pdf_path)
-    return content
-
-
 class UGentProgramSpider(scrapy.Spider, ABC):
     name = 'ugent-programs'
     custom_settings = {
@@ -87,35 +23,25 @@ class UGentProgramSpider(scrapy.Spider, ABC):
 
     def start_requests(self):
         faculties_codes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K']
-        i = 0
         for faculty_code in faculties_codes:
             for deg in ('BACH', 'MABA'):
                 yield scrapy.Request(
                     url=BASE_URL.format(year=YEAR, faculty=faculty_code, cycle=deg),
                     callback=self.parse_main
                 )
-            i += 1
-            if i >= 1:
-                break
 
     def parse_main(self, response):
         program_links = response.xpath("//li[a[@target='_top']]/a/@href").getall()
-        i = 0
         for link in program_links:
-            print(link)
             yield response.follow(link, self.parse_programmes)
-            i += 1
-            if i >= 1:
-                break
 
     def parse_programmes(self, response):
         program_id = response.url.split('/')[-2]
         program_name = response.xpath("//article//h1/text()").get()
         cycle = 'bac' if 'Bachelor' in program_name else 'master'
         faculty = response.xpath("//div/h2/text()").get()
-        # year = extract_year(cleanup(response.xpath("//h3").get()))
-        # version = cleanup(get_program_version(
-        #     response.xpath("//div[@class='menuHeader'][contains(text(), 'Programme')]").get()))
+        # TODO: make faculty a list?
+        faculty = faculty.split('\n')[0].strip(" \n")
         version = response.xpath("//div[@class='menuHeader'][contains(text(), 'Program')]/text()").get()
         version = version.split(' ')[-2].strip(")")
 
@@ -126,11 +52,8 @@ class UGentProgramSpider(scrapy.Spider, ABC):
                      'campus': '',  # Didn't find campus information
                      'url': response.url,
                      }
-                     # 'year': year,
-                     #'version(debug)': version}
 
         link = f"{response.url.split('.html')[0]}{version}(0)/{program_id}.html"
-        print(link)
         yield response.follow(link, self.parse_course_list, cb_kwargs={"base_dict": base_dict})
 
     def parse_course_list(self, response, base_dict):
@@ -138,14 +61,27 @@ class UGentProgramSpider(scrapy.Spider, ABC):
         # Get list of courses in this subprogram
         courses_ids = [url.split('/')[-1].strip('.pdf')
                        for url in response.xpath("//td[@class='cursus']//a/@href").getall()]
-        courses_ects = [int(e) for e in response.xpath("//tr[contains(@class, 'rowclass')]"
-                                                       "//td[@class='studiepunten']/text()").getall()]
-        print(courses_ids)
-        print(courses_ects)
+        courses_names = response.xpath("//td[@class='cursus']//a/text()").getall()
+        courses_ects = []
+        courses_teachers = []
+        for courses_id in courses_ids:
+            course_xpath = f"//td[@class='cursus']//a[contains(@href, '{courses_id}')]"
+            courses_ects += [response.xpath(f"{course_xpath}"
+                                            f"/following::td[@class='studiepunten'][1]/text()").get()]
+            courses_teachers += [response.xpath(f"{course_xpath}"
+                                                f"/following::td[@class='lesgever'][1]//text()").get()]
+        courses_ects = [float(e) if '.' in e else int(e) for e in courses_ects]
+        courses_teachers = [f"{' '.join(t.split(' ')[1:])} {t.split(' ')[0]}" if t is not None else None
+                            for t in courses_teachers]
+        print(len(courses_ids))
+        print(len(courses_ects))
+        print(len(courses_teachers))
         # If there are any, create an entry in the output file
         if len(courses_ids) != 0:
             cur_dict = {'courses': courses_ids,
-                        'ects': courses_ects}
+                        'ects': courses_ects,
+                        'courses_names': courses_names,
+                        'courses_teachers': courses_teachers}
             yield {**base_dict, **cur_dict}
 
         # Open subprograms
@@ -154,21 +90,3 @@ class UGentProgramSpider(scrapy.Spider, ABC):
         for subprogram in subprograms:
             url = '/'.join(response.url.split("/")[:-1]) + "/" + subprogram
             yield response.follow(url, self.parse_course_list, cb_kwargs={"base_dict": base_dict})
-
-    @staticmethod
-    def parse_course_list_final(response, base_dict):
-        urls = ["https://studiegids.ugent.be/" + cleanup(res) for res in response.xpath("//td[@class='cursus']/a/@href").getall()]
-        courses_id = [extract_id_from_url_ugent(url) for url in urls]
-        # courses_content = [extract_content(url) for url in urls]
-        courses_ects = extract_ects([cleanup(res) for res in response.xpath("//td[@class='studiepunten']").getall()])
-        courses_teacher = extract_teacher([cleanup(res) for res in response.xpath("//td[@class='lesgever']").getall()])
-        final_dict = {**base_dict,
-                      **{
-                          'url(debug)': response.url,
-                          'courses': courses_id,
-                          'courses_urls': urls,
-                          'courses_ects': courses_ects,
-                          'courses_teacher': courses_teacher,
-                          # 'courses_content': courses_content
-                      }}
-        yield final_dict
