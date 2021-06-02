01aa66518310c5c7fd1c98bf63ec5f97c63ab032
duboisa1 <antoine-dubois@hotmail.com>
2021-06-02 10:00:36 +0200

    Modified helmo and heff

--- src/crawl/unicrawl/spiders/he-ferrer_programs.py
+++ src/crawl/unicrawl/spiders/he-ferrer_programs.py
@@ -9,6 +9,13 @@ import scrapy
 from settings import YEAR, CRAWLING_OUTPUT_FOLDER
 
 BASE_URL = "https://fiches-ue.icampusferrer.eu/locked_list.php"
+FACULTIES_AND_CAMPUS = {"AA": ['Arts Appliqués', "Site Palais du Midi"],
+                        "Eco": ['Economique', 'Site Anneessens'],
+                        "ParaMed": ['Paramédical', 'Site Brugmann'],
+                        "Peda": ['Pédagogique', 'Site Lemonnier'],
+                        "Soc": ['Social', 'Site Anneessens'],
+                        "Tech": ['Technique', 'Site Palais du Midi']}
+PROGRAMS_FACULTIES_AND_CAMPUS = {"Arts du tissu": []}
 
 
 class HEFERRERProgramSpider(scrapy.Spider, ABC):
@@ -28,10 +35,14 @@ class HEFERRERProgramSpider(scrapy.Spider, ABC):
     @staticmethod
     def parse_main(response):
         ue_names = response.xpath("//tr//td[3]/text()").getall()
+        ue_links = response.xpath("//tr//td[1]/a/@href").getall()
+        print("aie")
+        print(ue_links)
         programs = response.xpath("//tr//td[4]/text()").getall()
         df = pd.DataFrame({'ue': ue_names, 'program': programs})
-        for program in sorted(list(set(programs))):
-            yield {"name": program,
-                   "id": "",
+        for i, program in enumerate(sorted(list(set(programs)))):
+            print(program)
+            yield {"id": i,
+                   "name": program,
                    "courses": df[df["program"] == program]['ue'].to_list()}
 
