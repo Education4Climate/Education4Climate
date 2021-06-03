b0ea7f17d2fe2877b50b357eba6c6f96eb1aca84
duboisa1 <antoine-dubois@hotmail.com>
2021-06-03 19:29:59 +0200

    He-ferrer done and crawled

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from os import path
 
 CRAWLING_OUTPUT_FOLDER = path.join('data', 'crawling-output', '')
 SCORING_OUTPUT_FOLDER = path.join('data', 'scoring-output', '')
-WEB_INPUT_FOLDER = path.join('docs','data', '')
-PATTERNS_PATH = path.join('data','patterns','themes_patterns.json')
+WEB_INPUT_FOLDER = path.join('docs', 'data', '')
+PATTERNS_PATH = path.join('data', 'patterns', 'themes_patterns.json')
 
 YEAR = "2020"
--- src/crawl/unicrawl/spiders/he-ferrer_programs.py
+++ src/crawl/unicrawl/spiders/he-ferrer_programs.py
@@ -9,13 +9,22 @@ import scrapy
 from settings import YEAR, CRAWLING_OUTPUT_FOLDER
 
 BASE_URL = "https://fiches-ue.icampusferrer.eu/locked_list.php"
-FACULTIES_AND_CAMPUS = {"AA": ['Arts Appliqués', "Site Palais du Midi"],
-                        "Eco": ['Economique', 'Site Anneessens'],
-                        "ParaMed": ['Paramédical', 'Site Brugmann'],
-                        "Peda": ['Pédagogique', 'Site Lemonnier'],
-                        "Soc": ['Social', 'Site Anneessens'],
-                        "Tech": ['Technique', 'Site Palais du Midi']}
-PROGRAMS_FACULTIES_AND_CAMPUS = {"Arts du tissu": []}
+SECTION_CAMPUS_PROGR = {"AA": ['Arts Appliqués', 'Site Palais du Midi',
+                               ["Arts du tissu", "Publicité", "Styliste et modéliste"]],
+                        "EcoB": ['Economique', 'Site Anneessens',
+                                 ["Assistant de direction", "Comptabilité",
+                                  "Management de la logistique", "Sciences administratives et gestion publique"]],
+                        "EcoM": ['Economique', 'Site Anneessens',
+                                 ["Agrégation de l'enseignement secondaire supérieur", "Gestion de l'entreprise",
+                                  "Gestion publique", "Ingénieur commercial", "Master en sciences administratives",
+                                  "Master en sciences commerciales"]],
+                        "ParaMed": ['Paramédical', 'Site Brugmann',
+                                    ["Biologie médicale", "Sage-femme", "Soins infirmiers"]],
+                        "Peda": ['Pédagogique', 'Site Lemonnier',
+                                 ["Normale Préscolaire", "Normale primaire", "Normale secondaire",
+                                  "Préparation physique et entraînement"]],
+                        "Soc": ['Social', 'Site Anneessens', ["Gestion des ressources humaines"]],
+                        "Tech": ['Technique', 'Site Palais du Midi', ["Electronique",  "Techniques graphiques"]]}
 
 
 class HEFERRERProgramSpider(scrapy.Spider, ABC):
@@ -32,17 +41,23 @@ class HEFERRERProgramSpider(scrapy.Spider, ABC):
     def start_requests(self):
         yield scrapy.Request(BASE_URL, self.parse_main)
 
+    # TODO: add cycle
     @staticmethod
     def parse_main(response):
-        ue_names = response.xpath("//tr//td[3]/text()").getall()
-        ue_links = response.xpath("//tr//td[1]/a/@href").getall()
-        print("aie")
-        print(ue_links)
-        programs = response.xpath("//tr//td[4]/text()").getall()
-        df = pd.DataFrame({'ue': ue_names, 'program': programs})
-        for i, program in enumerate(sorted(list(set(programs)))):
-            print(program)
-            yield {"id": i,
-                   "name": program,
-                   "courses": df[df["program"] == program]['ue'].to_list()}
+        ue_codes = [url.split("id=")[1].split("&")[0] for url in response.xpath("//tr//td[1]/a/@href").getall()]
+        sub_programs = response.xpath("//tr//td[4]/text()").getall()
+        df = pd.DataFrame({'ue': ue_codes, 'program': sub_programs})
+        for section_code, (section, campus, main_programs) in SECTION_CAMPUS_PROGR.items():
+            for i, main_program in enumerate(main_programs):
+                associated_sub_programs = sorted(list(set(df[df["program"].str.contains(main_program)]["program"])))
+                for j, sub_program in enumerate(associated_sub_programs):
+                    cycle = 'bac'
+                    if section_code == 'EcoM' and ('Master' in sub_program or not sub_program.endswith(" - ")):
+                        cycle = 'master'
+                    yield {"id": f"{section_code}-{i}-{j}",
+                           "name": sub_program.strip(" - "),
+                           "faculty": section,
+                           "campus": campus,
+                           "cycle": cycle,
+                           "courses": df[df["program"] == sub_program]['ue'].to_list()}
 
