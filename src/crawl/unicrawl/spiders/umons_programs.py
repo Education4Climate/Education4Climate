e285605fc8612f37ac3c41a9bbc43e1cf753f711
duboisa1 <antoine-dubois@hotmail.com>
2021-09-03 19:02:57 +0200

    Updated UMons Crawler

--- src/crawl/unicrawl/spiders/umons_programs.py
+++ src/crawl/unicrawl/spiders/umons_programs.py
@@ -5,10 +5,19 @@ from pathlib import Path
 
 from src.crawl.utils import cleanup
 from settings import YEAR, CRAWLING_OUTPUT_FOLDER
-BASE_URL = "https://web.umons.ac.be/fr/enseignement/loffre-de-formation-de-lumons/"
+
+BASE_URL = "https://web.umons.ac.be/fr/formations/"
+BASE_DATA = {
+    "degree[]": "bachelor",
+    "schedule[]": ["daytime", "adapted", "alternate"],
+    "complete": "1",
+    "search": "",
+    "search-trainings": "1",
+}
 
 
 class UMonsProgramSpider(scrapy.Spider, ABC):
+
     name = "umons-programs"
     custom_settings = {
         'FEED_URI': Path(__file__).parent.absolute().joinpath(
@@ -16,36 +25,51 @@ class UMonsProgramSpider(scrapy.Spider, ABC):
     }
 
     def start_requests(self):
-        yield scrapy.Request(url=BASE_URL, callback=self.parse_offer)
+        # yield scrapy.Request(url=BASE_URL, callback=self.parse_offer)
 
-    def parse_offer(self, response):
+        cycle_url_codes = ["bachelor", "master", "specialization", "phd",
+                           "aess", "capaes", "certificate", "interuniversity-certificate"]
+        cycle_codes = ["bac", "master", "master", "doctor",
+                       "other", "certificate", "certificate", "certificate"]
 
-        formations = response.xpath('//span//a[not(contains(@href, "png"))]')
-        urls = formations.xpath('@href').getall()
-        cycles = formations.xpath('text()').getall()
-        assert len(urls) == len(cycles)
-        for cycle, url in zip(cycles, urls):
-            if 'BA' in cycle:
-                cycle = 'bac'
-            elif 'MA' in cycle:
-                cycle = 'master'
+        for url_code, code in zip(cycle_url_codes, cycle_codes):
+
+            BASE_DATA["degree[]"] = url_code
+
+            # Number of results is limited to 40 and there are more than 40 master
+            # So divide the search by faculty
+            if url_code == "master":
+                for faculty_code in ["2", "3", "5", "6", "7", "8", "9", "10", "11"]:
+                    base_data_with_faculty = BASE_DATA.copy()
+                    base_data_with_faculty["entity[]"] = faculty_code
+                    yield scrapy.http.FormRequest(
+                        BASE_URL,
+                        callback=self.parse_offer,
+                        formdata=base_data_with_faculty,
+                        cb_kwargs={"cycle": code}
+                    )
             else:
-                cycle = 'other'
-            yield response.follow(url, self.parse_prog, cb_kwargs={'cycle': cycle})
+                yield scrapy.http.FormRequest(
+                    BASE_URL,
+                    callback=self.parse_offer,
+                    formdata=BASE_DATA,
+                    cb_kwargs={"cycle": code}
+                )
 
-    def parse_details(self, response, cycle):
+    def parse_offer(self, response, cycle):
 
-        programs = response.xpath(
-            '//article[starts-with(@class,"shortcode-training training-small scheme-")]/a/@href').getall()
-        for program in programs:
-            yield response.follow(program, self.parse_prog, cb_kwargs={'cycle': cycle})
+        program_links = response.xpath("//a[header]/@href").getall()
+        print(cycle, len(program_links))
+        for link in program_links:
+            yield response.follow(link, self.parse_prog, cb_kwargs={'cycle': cycle})
 
     def parse_prog(self, response, cycle):
 
         href = response.xpath(
             '//a[contains(@class, "button-primary-alt scheme-background scheme-background-hover")]/@href').get()
         if not href:
-            yield response.follow(response.url, self.parse_details, cb_kwargs={'cycle': cycle})
+            # No program details and no program id
+            return
         else:
             campus = response.xpath('//div[div/text()="Lieu"]').css('div.value::text').get()
             if 'mailto' in href:
@@ -62,7 +86,9 @@ class UMonsProgramSpider(scrapy.Spider, ABC):
     def parse_prog_detail(response, campus, cycle, url):
 
         faculty = response.css('td.facTitle::text').get()
-        program_name = response.css('td.cursusTitle::text').get()
+        program_name = response.css('td.cursusTitle::text').get().replace("  ", " ")
+        if not program_name:
+            return
         courses = response.css('span.linkcodeue::text').getall()
         courses = [course.strip(" ") for course in courses]
         ects = cleanup(response.xpath("//td[@class='credits colnumber']").getall())
