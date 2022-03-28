4285c10504f70ad12ae4d44c43a90937c979b6f3
duboisa1 <antoine-dubois@hotmail.com>
2022-03-28 16:11:21 +0200

    Added HOGENT (crawled, scored and web)

---
+++ src/crawl/unicrawl/spiders/hogent_courses.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from abc import ABC
+from pathlib import Path
+
+import pandas as pd
+import scrapy
+
+from src.crawl.utils import cleanup
+from settings import YEAR, CRAWLING_OUTPUT_FOLDER
+
+BASE_URL = "https://bamaflexweb.hogent.be/BMFUIDetailxOLOD.aspx?a={}&b=5&c=1"  # format brackets will hold code course
+PROG_DATA_PATH = Path(__file__).parent.absolute().joinpath(
+    f'../../../../{CRAWLING_OUTPUT_FOLDER}hogent_programs_{YEAR}.json')
+
+LANGUAGES_DICT = {
+    "Frans": 'fr',
+    "Engels": 'en',
+    "Nederlands": 'nl',
+    "Spaans": "es",
+    "Duits": "de",
+    "Chinees": "cn",
+    "Mandarijn Chinees": "cn"
+}
+
+
+class HOGENTCourseSpider(scrapy.Spider, ABC):
+    """
+    Courses crawler for HOGENT
+    """
+
+    name = "hogent-courses"
+    custom_settings = {
+        'FEED_URI': Path(__file__).parent.absolute().joinpath(
+            f'../../../../{CRAWLING_OUTPUT_FOLDER}hogent_courses_{YEAR}.json')
+    }
+
+    def start_requests(self):
+
+        courses_ids = pd.read_json(open(PROG_DATA_PATH, "r"))["courses"]
+        courses_ids_list = sorted(list(set(courses_ids.sum())))
+
+        for course_id in courses_ids_list:
+            yield scrapy.Request(
+                url=BASE_URL.format(course_id),
+                callback=self.parse_course,
+                cb_kwargs={"course_id": course_id},
+            )
+
+    @staticmethod
+    def parse_course(response, course_id):
+
+        body = response.css("#content")
+        name = body.css("h2::text").get()
+        year = body.css("#ctl00_ctl00_cphGeneral_cphMain_lblAcademiejaarOmschrijving::text").get()
+
+        teachers = body.xpath("//span[text()='Coördinator: ' or text()='Docenten: ' "
+                              "or text()='Andere docenten: ']/following::span[1]/text()").getall()
+        teachers = ",".join(teachers).strip(", ")
+        teachers = [t.strip() for t in teachers.split(",")] if teachers else []
+
+        languages = body.xpath("//span[text()='Onderwijstalen: ']/following::span[1]/text()").get()
+        print(languages)
+        if languages:
+            languages = [lang.strip() for lang in languages.split(',')]
+            languages = [lang for lang in languages if lang != 'Taaloverschrijdend']
+            languages = [LANGUAGES_DICT[lang] if lang in LANGUAGES_DICT else 'other' for lang in languages]
+            languages = ["nl"] if len(languages) == 0 else languages
+        else:
+            languages = ['nl']
+        print(languages)
+
+        content = cleanup(body.xpath("//h4[contains(text(),'Inhoud')]/following::div[1]").xpath("string(.)").get())
+
+        yield {
+            'id': course_id,
+            'name': name,
+            'year': year,
+            'languages': languages,
+            'teachers': teachers,
+            'url': response.url,
+            'content': content,
+            'goal': '',
+            'activity': '',
+            'other': ''
+        }
---
+++ src/crawl/unicrawl/spiders/hogent_programs.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+from abc import ABC
+from pathlib import Path
+import json
+
+import scrapy
+
+from settings import YEAR, CRAWLING_OUTPUT_FOLDER
+
+BASE_URL = f"https://studiefiches.hogent.be/index.cfm/fetch-study-programs?" \
+           f"format=json&acadyear={YEAR}-{YEAR-2000+1}&lang=1"
+
+PROGRAM_URL = f"https://studiefiches.hogent.be/index.cfm/modeltrajecten?" \
+              f"acadyear={YEAR}-{YEAR-2000+1}" + "&opl={}&dep={}&lang=1"
+
+MODEL_TRAJECT_URL = f"https://studiefiches.hogent.be/index.cfm/fetch-model-trajects?" \
+                    f"format=json&acadyear={YEAR}-{YEAR-2000+1}" + "&opl={}&dep={}&lang=1"
+
+COURSES_LIST_URL = "https://studiefiches.hogent.be/index.cfm/fetch-opleidingsonderdelen?format=json&mid={}" \
+                   "&opl={}&dep={}" + f"&acadyear={YEAR}-{YEAR-2000+1}&lang=1"
+
+FACULTIES_DICT = {
+    'DBO': 'Departement Bedrijf en Organisatie',
+    'DBT': 'Departement Biowetenschappen en Industriële Technologie',
+    'DGZ': 'Departement Gezondheidszorg',
+    'DIT': 'Departement IT en Digitale Innovatie',
+    'DLO': 'Departement Lerarenopleiding',
+    'DOG': 'Departement Omgeving',
+    'DSA': 'Departement Sociaal-Agogisch Werk',
+    'GO5': 'GO5 by HOGENT',
+    'SCH': 'School of Arts KASK-Koninklijk Conservatorium'
+}
+
+CYCLES_DICT = {
+    'ABA': 'bac',
+    'BIJNA': 'other',
+    'BNB': 'bac',
+    'GRAD': 'grad',
+    'GRDU': 'grad',
+    'INT': 'other',
+    'MA': 'master',
+    'MNM': 'master',
+    'PBA': 'bac',
+    'PGR': 'postgrad'
+}
+
+
+class HOGENTProgramSpider(scrapy.Spider, ABC):
+    """
+    Programs crawler for HOGENT
+    """
+
+    name = "hogent-programs"
+    custom_settings = {
+        'FEED_URI': Path(__file__).parent.absolute().joinpath(
+            f'../../../../{CRAWLING_OUTPUT_FOLDER}hogent_programs_{YEAR}_pre.json')
+    }
+
+    def start_requests(self):
+
+        yield scrapy.Request(BASE_URL, self.parse_main)
+
+    def parse_main(self, response):
+
+        programs_list = response.json()
+        for program_dict in programs_list:
+            main_program_name = program_dict["OPLEIDING"]
+            main_program_id = program_dict["OPLEIDINGCODE"]
+            fac_code = program_dict["DEPARTEMENTCODE"]  # = FACULTEITCODE
+            cycle = CYCLES_DICT[program_dict["BAMATYPEKORT"]]
+            yield response.follow(MODEL_TRAJECT_URL.format(main_program_id, fac_code), self.parse_program,
+                                  cb_kwargs={'main_program_name': main_program_name,
+                                             'main_program_id': main_program_id,
+                                             'fac_code': fac_code,
+                                             'cycle': cycle})
+
+    def parse_program(self, response, main_program_name, main_program_id, fac_code, cycle):
+
+        # Convert faculty code to name
+        faculties = [FACULTIES_DICT[fac_code]]
+
+        trajects = response.json()
+
+        for traject_id, traject in enumerate(trajects):
+            traject_name = traject['DEELTRAJECT']
+
+            program_id = f"{main_program_id}-{traject_id}"
+            program_name = main_program_name + ' - ' + traject_name.title()
+            for sub_traject in traject['ARRDEELTRAJECTEN']:
+                sub_traject_id = sub_traject['modeltrajectid']
+
+                base_dict = {
+                    "id": program_id,
+                    "name": program_name,
+                    "cycle": cycle,
+                    "faculties": faculties,
+                    "campuses": [],
+                    "url": PROGRAM_URL.format(main_program_id, fac_code)
+                }
+
+                yield response.follow(COURSES_LIST_URL.format(sub_traject_id, main_program_id, fac_code),
+                                      self.parse_courses, cb_kwargs={'base_dict': base_dict})
+
+    @staticmethod
+    def parse_courses(response, base_dict):
+
+        # Could have been used if everything was not only in the xhr
+        # Avoid course that precede an enumeration (e.g. choice between several courses)
+        # print(response.xpath("//tr[not(following-sibling::tr[1]//li and child::td/a)]//a").getall())
+        courses = []
+        ects = []
+        for course_dict in response.json():
+            courses += [str(course_dict["OPLEIDINGSONDERDEELID"])]
+            ects += [int(float(course_dict["STUDIEPUNTEN"]))]
+
+        base_dict["courses"] = courses
+        base_dict["ects"] = ects
+
+        yield base_dict
