73688bfe9b6e8fe8ef5f1e3941cdb103f82df23d
duboisa1 <antoine-dubois@hotmail.com>
2023-11-30 22:18:33 +0100

    HELDB - 2022 update + 2023

--- src/crawl/unicrawl/spiders/heldb_programs.py
+++ src/crawl/unicrawl/spiders/heldb_programs.py
@@ -6,15 +6,11 @@ import scrapy
 
 from settings import YEAR, CRAWLING_OUTPUT_FOLDER
 
-BASE_URL = "https://www.heldb.be/wpfiches/#"
+BASE_URL = "https://www.heldb.be/he-grilles"
 DEPARTMENTS_NAMES = {
     0: "Sciences économiques et de gestion",
     1: "Sciences de l'éducation",
     2: "Sciences et techniques"
-}
-
-ACAD_YEARS_CODES = {
-    2022: '9'
 }
 
 
@@ -26,7 +22,7 @@ class HELDBProgramSpider(scrapy.Spider, ABC):
     name = "heldb-programs"
     custom_settings = {
         'FEED_URI': Path(__file__).parent.absolute().joinpath(
-            f'../../../../{CRAWLING_OUTPUT_FOLDER}heldb_programs_{YEAR}_pre.json').as_uri()
+            f'../../../../{CRAWLING_OUTPUT_FOLDER}heldb_programs_{YEAR}.json').as_uri()
     }
 
     def start_requests(self):
@@ -35,56 +31,50 @@ class HELDBProgramSpider(scrapy.Spider, ABC):
     def parse_main(self, response):
 
         # Get links to programs
-        programs_links = response.xpath("//div[@class='dropdown-menu']/a/@href").getall()
-        programs_campuses = response.xpath("//div[@class='dropdown-menu']/a/span/text()").getall()
-        for link, campus in zip(programs_links, programs_campuses):
-            yield response.follow(link + ACAD_YEARS_CODES[YEAR], self.parse_program, cb_kwargs={'campus': campus})
+        programs_links = response.xpath("//body/div[1]//tr//td//a[1]/@href").getall()
+        for link in programs_links:
+            yield response.follow(link, self.parse_program)
 
     @staticmethod
-    def parse_program(response, campus):
+    def parse_program(response):
 
-        program_id = response.url.split("sodid=")[1].split("&")[0]
-        if len(program_id) == 0:
+        program_id = response.url.split("/")[-1]
+        program_name = response.xpath("//body/div[1]//strong[contains(text(), 'Section')]"
+                                      "/following::text()[1]").get().strip(" ")
+        if not program_name:
+            print(f"No program name available for {response.url}")
             return
-        program_name = response.xpath("//b[contains(text(), 'Section')]/following::text()[1]").get()
-        program_subname = response.xpath("//b[contains(text(), 'Option')]/following::text()[1]").get()
+        program_subname = response.xpath("//body/div[1]//strong[contains(text(), 'Orientation')]"
+                                         "/following::text()[1]").get()
         if program_subname:
             program_subname = program_subname.strip('\n ')
             # Some programs do not have options
             if len(program_subname) != 0:
                 program_name += f" - {program_subname}"
-        # Special case for 'Sciences de l'Ingénieur Industriel'
-        if "Sciences de l'Ingénieur Industriel" in program_name:
-            sub_programs = response.xpath("//b[contains(text(), 'Option')]/following::text()[1]").getall()
-            if any(['Biochimie' in sub_program for sub_program in sub_programs]):
-                program_name = "Sciences de l'Ingénieur Industriel - Biochimie"
-                program_id = "24"
-            else:
-                program_name = "Sciences de l'Ingénieur Industriel - Chimie"
-                program_id = "27"
 
-        bloc = response.xpath("//th[contains(text(), 'BLOC')]/text()").get()
+        bloc = response.xpath("//body/div[1]//td[contains(text(), 'BLOC')]/text()").get()
         bloc_number = int(bloc.split(" ")[1])
         cycle = 'bac' if bloc_number < 4 else 'master'
 
-        faculty = response.xpath("//b[contains(text(), 'Domaine')]/following::text()[1]").get()
+        faculty = response.xpath("//body/div[1]//strong[contains(text(), 'Domaine')]/following::text()[1]").get()
 
-        main_text = "//div[contains(@class, 'container-fluid')]"
-        ue_ids = response.xpath(f"{main_text}//tr[contains(@class, 'ue')"
-                                f" and contains(@class, 'bold')]/td[2]/text()").getall()
+        main_text = '//body/div[1]'
+        ue_ids = response.xpath(f"{main_text}//tr[contains(@class, 'ue') and"
+                                f" td[contains(@class, 'bold')]]/td[2]/text()").getall()
         ue_ids = [ue.strip("\n ") for ue in ue_ids]
-        ue_ects = response.xpath(f"{main_text}//tr[contains(@class, 'bold')]/td[5]/text()").getall()
-        ue_urls_ids = response.xpath(f"{main_text}//tr[contains(@class, 'ue')]/@data-href").getall()
+        ue_ects = response.xpath(f"{main_text}//tr[contains(@class, 'ue') and"
+                                 f" td[contains(@class, 'bold')]]/td[4]/text()").getall()
+        ue_ects = [int(e) for e in ue_ects]
+        ue_urls_ids = response.xpath(f"{main_text}//tr[contains(@class, 'ue') and"
+                                     f" td[contains(@class, 'bold')]]/@data-href").getall()
         ue_urls_ids = [ue.split("/")[-1] for ue in ue_urls_ids]
-
-        # ue_ids, ue_ects, ue_urls_ids = list(zip(*set(zip(ue_ids, ue_ects, ue_urls_ids))))
 
         yield {
             "id": program_id,
             "name": program_name,
             "cycle": cycle,
             "faculties": [faculty],
-            "campuses": [campus],
+            "campuses": [],
             "url": response.url,
             "courses": ue_ids,
             "ects": ue_ects,
